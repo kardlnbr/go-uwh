216b73742dfc5c61f66655ef0af43278f326d4ae
kardlnbr <kkardelenn38@gmail.com>
2024-10-28 03:07:24 +0300

    Create QuadA.go

---
+++ QuadA.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func QuadA(x, y int) {
+	var result rune
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+			if (i == 1 && j == 1) || (i == y && j == 1) || (i == 1 && j == x) || (i == y && j == x) {
+				result = 'o'
+			} else if i == 1 || i == y {
+				result = '-'
+			} else if j == 1 || j == x {
+				result = '|'
+			} else {
+				result = ' '
+			}
+			fmt.Printf("%c", result)
+		}
+		fmt.Println()
+	}
+
+}
+
+func main() {
+	QuadA(1,5)
+}
