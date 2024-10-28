ed6bc47bddc808c86ea654ce1a3e892f5b34233d
Kardelen Kılıç <kkardelenn38@gmail.com>
2024-10-28 09:05:42 +0300

    Update QuadE.go

--- QuadE.go
+++ QuadE.go
@@ -1,25 +1,32 @@
 package main
-import ("fmt")
 
-func QuadE(x,y int){
+import "fmt"
+
+func QuadA(x, y int) {
 	var result rune
-for i:=1; i<=y; i++{
-	for j:=1; j<=x; j++{                                
-	if (i==1 && j==x) || (i==y && j==1){
-		result= 'C'
-	} else if (i==1 && j==1) || (i==y && j==x){
-		result= 'A'
-	}else if j == 1 || j == x || i == 1 || i == y{
-		result= 'B'
-	}else {
-		result= ' '
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+			if i == 1 && j == 1 {
+				result = 'A'
+			} else if i == 5 && j == 1 {
+				result = 'C'
+			} else if (i == 1 && j == x) || (i == y && j == 1) {
+				result = 'C'
+			} else if i == 1 || i == y {
+				result = 'B'
+			} else if j == 1 || j == x {
+				result = 'B'
+			} else {
+				result = ' '
+			}
+			fmt.Printf("%c", result)
+		}
+		fmt.Println()
 	}
-	fmt.Printf ("%c", result)
-}
-fmt.Println()
-}
+
 }
 
 func main() {
-	QuadE(5,3)
-}
+	QuadA(5, 3)
+})
+
